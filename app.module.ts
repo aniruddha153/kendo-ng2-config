0315a1868b929bdcb096369d39c07a84a2ff39ad
Aniruddha Pondhe <aniruddha153@gmail.com>
2016-11-24 18:58:03 +0530

    Updated comments

--- app.module.ts
+++ app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+//Importing Kendo Ng2 Buttons Component
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
 import { AppComponent } from './app.component';
 
 @NgModule({
     bootstrap: [AppComponent],
     declarations: [AppComponent],
-    imports: [BrowserModule, ButtonsModule]
+    imports: [BrowserModule,
+              ButtonsModule] //Bootstraping Kendo Ng2 Buttons Module
 })
 export class AppModule {
 }
